e7f98c4d390dff19e9a313e0c71108476df3dfba
plakapenka <plakapenok1@gmail.com>
2023-04-12 02:12:41 +0300

    Revert "temp toner color changer"
    This reverts commit b967ab877ecfa42a4da7d1bb009502f93f8dbf32.

--- app/src/main/cpp/libsamp/game/hooks.cpp
+++ app/src/main/cpp/libsamp/game/hooks.cpp
@@ -1651,8 +1651,7 @@ RpMaterial* CVehicle__SetupRenderMatCB(RpMaterial* material, void* data)
 				}
 			}
 		}
-		if(material->color.red == 255 && material->color.green == 255 && material->color.blue == 0)
-		{// toner
+		if(material->color.red == 255 && material->color.green == 255 && material->color.blue == 0){
 			resetEntriesVehicle.emplace_back(reinterpret_cast<unsigned int*>(&(material->color)), *reinterpret_cast<unsigned int*>(&(material->color)));
 			material->color.alpha = pVeh->toner.a;
 			material->color.green = pVeh->toner.g;

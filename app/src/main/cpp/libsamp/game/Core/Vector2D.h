df10244863cddded2ec537d3cb9189c769d83e73
plakapenka <plakapenok1@gmail.com>
2023-04-19 12:08:37 +0300

    CRect

--- app/src/main/cpp/libsamp/game/Core/Vector2D.h
+++ app/src/main/cpp/libsamp/game/Core/Vector2D.h
@@ -4,8 +4,14 @@
 
 #pragma once
 
-typedef struct _CVector2D
-{
-    int X;
-    int Y;
-} CVector2D;
+#include "../RW/rwlpcore.h"
+
+class CVector2D : public RwV2d {
+
+public:
+    constexpr CVector2D() = default;
+    constexpr CVector2D(float XY) : RwV2d{XY, XY} {}
+    constexpr CVector2D(float X, float Y) : RwV2d{ X, Y } {}
+    constexpr CVector2D(const RwV2d& vec2d)     { x = vec2d.x; y = vec2d.y; }
+    constexpr CVector2D(const CVector2D& vec2d) { x = vec2d.x; y = vec2d.y; }
+};
